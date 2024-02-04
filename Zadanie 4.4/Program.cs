b1a1bf0c54fc0f401f8650616f4fdb8eb5179797
Romeq03 <romekbartek18@gmail.com>
2024-02-04 21:45:07 +0100

    first commit

---
+++ Zadanie 4.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList listaMiast = new ArrayList();
+
+        listaMiast.Add("Gdańsk");
+        listaMiast.Add("Warszawa");
+        listaMiast.Add("Katowice");
+        listaMiast.Add("Poznań");
+        listaMiast.Add("Kraków");
+        listaMiast.Add("Rzeszów");
+
+        listaMiast.Sort();
+
+        Console.WriteLine("Posortowane miasta:");
+        foreach (string miasto in listaMiast)
+        {
+            Console.WriteLine(miasto);
+        }
+
+        Console.ReadLine();
+    }
+}
